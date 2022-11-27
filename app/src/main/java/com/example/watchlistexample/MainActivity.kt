f90cbeaa2fb9c5d96211b0dad5fb9dd87a482ce2
tim_heung <tim_heung@chiefgroup.com.hk>
2022-11-27 13:15:35 +0800

    feat: add navigation compose for bottom bar, change to use material instead of material 3, update main screen ui

--- app/src/main/java/com/example/watchlistexample/MainActivity.kt
+++ app/src/main/java/com/example/watchlistexample/MainActivity.kt
@@ -3,38 +3,103 @@ package com.example.watchlistexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.watchlistexample.ui.theme.WatchlistExampleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WatchlistExampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+            HomeScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomeScreen(){
+    val navController = rememberNavController()
+    WatchlistExampleTheme {
+        // A surface container using the 'background' color from the theme
+        Scaffold(
+            topBar = {
+                TopAppBar( title = { Text(text = "Watchlist") },
+                    // title with watchtlist and search icon at right
+
+                    actions = {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(Icons.Default.Search, contentDescription = "Search")
+                        }
+                    })
+            },
+            bottomBar = {
+                BottomNavigation(navController = navController)
+            },
+            content = { padding ->
+                Surface(modifier = Modifier.padding(padding), color = MaterialTheme.colors.background) {
+
+                }
+            }
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    WatchlistExampleTheme {
-        Greeting("Android")
+fun BottomNavigation(navController: NavController) {
+    val items = listOf(
+        BottomNavItem("Market", Icons.Default.AreaChart, "market", "market"),
+        BottomNavItem("Market", Icons.Default.AccountBalanceWallet, "market" , "market2"),
+        BottomNavItem("Market", Icons.Default.ChatBubbleOutline, "market", "market3"),
+        BottomNavItem("Market", Icons.Default.AccountCircle, "market", "market4"),
+    )
+    BottomNavigation(
+        contentColor = Color.Black
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = { Icon(imageVector = item.icon, contentDescription = item.title) },
+                label = { Text(text = item.title,
+                    fontSize = 9.sp) },
+                selectedContentColor = Color.Black,
+                unselectedContentColor = Color.Black.copy(0.4f),
+                alwaysShowLabel = true,
+                selected = currentRoute == item.screenRoute,
+                onClick = {
+                    navController.navigate(item.screenRoute) {
+
+                        navController.graph.startDestinationRoute?.let { screen_route ->
+                            popUpTo(screen_route) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
     }
+}
+
+data class BottomNavItem(val title:String, val icon:ImageVector, val contentDescription: String, val screenRoute:String)
+
+@Preview
+@Composable
+fun HomeScreenPreview(){
+    HomeScreen()
 }
