6d978873d64f1e8eb21e922b5e89154c186c1966
tim_heung <tim_heung@chiefgroup.com.hk>
2022-11-28 17:32:47 +0800

    feat: add watchlist item view

--- app/src/main/java/com/example/watchlistexample/ui/view/WatchlistScreen.kt
+++ app/src/main/java/com/example/watchlistexample/ui/view/WatchlistScreen.kt
@@ -2,32 +2,53 @@ package com.example.watchlistexample.ui.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import java.math.BigDecimal
 
 @Composable
 fun WatchlistScreen() {
     Surface {
-        Text(text = "Watchlist")
+        Column {
+            SummaryView()
+            WatchlistHeaderCellView()
+            LazyColumn {
+
+                items(20) {
+
+                    WatchlistItemView()
+                }
+
+            }
+        }
     }
 
+}
+
+@Preview
+@Composable
+fun WatchlistScreenPreview(){
+    WatchlistScreen()
 }
 
 
 @Composable
 fun SummaryViewItem(modifier: Modifier = Modifier) {
-    ConstraintLayout(modifier = Modifier.fillMaxWidth().then(modifier)) {
+    ConstraintLayout(modifier = Modifier
+        .fillMaxWidth()
+        .then(modifier)) {
         val (title1, value1, title2, value2) = createRefs()
         Text(
             overflow = TextOverflow.Ellipsis,
@@ -83,30 +104,31 @@ fun SummaryViewItem(modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun SummaryViewItemPreview2() {
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .padding(10.dp)
-            .height(40.dp)
-            .clip(RoundedCornerShape(4.dp)).background(Color.Blue)) {
-            Box(modifier = Modifier
-                .weight(1f)
-                .background(Color.Red)) {
-                SummaryViewItem()
-            }
-            Divider(modifier = Modifier
-                .padding(4.dp)
-                .fillMaxHeight()
-                .width(1.dp)
-
-                .background(Color.Gray))
-            Box(modifier = Modifier
-                .weight(1f)
-                .background(Color.Green)) {
-                SummaryViewItem()
-            }
-
+fun SummaryView() {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(10.dp)
+        .height(40.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .background(Color.Blue)) {
+        Box(modifier = Modifier
+            .weight(1f)
+            .background(Color.Red)) {
+            SummaryViewItem()
         }
+        Divider(modifier = Modifier
+            .padding(4.dp)
+            .fillMaxHeight()
+            .width(1.dp)
+
+            .background(Color.Gray))
+        Box(modifier = Modifier
+            .weight(1f)
+            .background(Color.Green)) {
+            SummaryViewItem()
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
@@ -151,19 +173,27 @@ fun SummaryViewPreview2() {
 }
 
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun WatchlistHeaderCellView() {
     Column {
         Row(horizontalArrangement = Arrangement.SpaceEvenly,
             modifier = Modifier
                 .fillMaxWidth()
-                .height(IntrinsicSize.Max)
+                .height(50.dp)
                 .background(Color.White)) {
-            Text(text = "Symbol")
-            Text(text = "Change")
-            Text(text = "Sell")
-            Text(text = "Buy")
+            Text(modifier = Modifier
+                .weight(0.25f)
+                .fillMaxHeight(), textAlign = TextAlign.Center, color = Color.Black,text = "Symbol")
+            Text(modifier = Modifier
+                .weight(0.25f)
+                .fillMaxHeight(), textAlign = TextAlign.Center, color = Color.Black,text = "Change")
+            Text(modifier = Modifier
+                .weight(0.25f)
+                .fillMaxHeight(), textAlign = TextAlign.Center, color = Color.Black,text = "Sell")
+            Text(modifier = Modifier
+                .weight(0.25f)
+                .fillMaxHeight(), textAlign = TextAlign.Center, color = Color.Black,text = "Buy")
         }
         Divider(Modifier
             .fillMaxWidth()
@@ -172,3 +202,33 @@ fun WatchlistHeaderCellView() {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun WatchlistItemView(item: WatchlistItem = WatchlistItem("AAPL", BigDecimal(10.0), BigDecimal(11.0), BigDecimal(11.0), BigDecimal(10.0))) {
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .background(Color.White)) {
+            Text(modifier = Modifier
+                .weight(0.25f)
+                .fillMaxHeight(), textAlign = TextAlign.Center, color = Color.Black, text = item.symbol)
+            Text(modifier = Modifier
+                .weight(0.25f)
+                .fillMaxHeight(), textAlign = TextAlign.Center, color = Color.Black, text = item.getChange().toString())
+            Text(modifier = Modifier
+                .weight(0.25f)
+                .fillMaxHeight(), textAlign = TextAlign.Center, color = Color.Black, text = item.sell.toString())
+            Text(modifier = Modifier
+                .weight(0.25f)
+                .fillMaxHeight(), textAlign = TextAlign.Center, color = Color.Black, text = item.buy.toString())
+        }
+    }
+}
+
+data class WatchlistItem(val symbol: String, val prev: BigDecimal, val sell: BigDecimal, val buy: BigDecimal, val price: BigDecimal)
+
+fun WatchlistItem.getChange(): BigDecimal {
+    return (price - prev) / prev
+}
